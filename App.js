b445fd2199ed0f1b8ec051fc5f39fad2ca8729af
Abhinav18042009 <abhinavnori1804@gmail.com>
2021-06-17 15:15:04 +0530

    App complete

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import TransactionScreen from './bookTransactionScreen';
+import SearchScreen from './searchScreen';
+import LoginScreen from './LoginScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <AppContainer />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+var TabNavigator = createBottomTabNavigator({
+  Transaction : {screen:TransactionScreen},
+  Search:{screen:SearchScreen}
+},
+{
+defaultNavigationOptions:({navigation}) => ({
+  tabBarIcon:({}) => {
+    const routeName = navigation.state.routeName;
+    if(routeName === 'Transaction'){
+      return(
+        <Image
+        source = {require('./assets/book.png')}
+        style = {{
+          width:40,
+          height:40,
+        }} />
+      )
+    }
+    else if(routeName === 'Search'){
+      return(
+        <Image
+        //source = {require('./assets/searchingBook.png')}
+        style = {{
+          width:40,
+          height:40,
+        }} />
+      )
+    } 
+  }
+})}
+)
+
+const SwitchNavigator = createSwitchNavigator({
+  Login : {screen:LoginScreen},
+  Tabs : {screen:TabNavigator}
+})
+
+const AppContainer = createAppContainer(SwitchNavigator);
